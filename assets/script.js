f861a365bf58dfa3d02e6e42852de014b0917a48
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-14 19:05:08 -0700

    Printing to the page via javascript

--- assets/script.js
+++ assets/script.js
@@ -15,11 +15,15 @@ var humidity;
 var windSpeed;
 var UVindex; 
 
+var currentTitle = document.querySelector('.current-city'); 
+var current = document.querySelector('.weather');
 
 
 // on submit for the city
 submitBtn.addEventListener('click', function(event){
     event.preventDefault();
+    currentTitle.style.display = "block"; 
+    current.style.display = "block";
     city = cityContent.value; 
     searchHistory.push(city); 
     latLong();
@@ -28,10 +32,11 @@ submitBtn.addEventListener('click', function(event){
 
 //var apiWeatherKey = c99c17905b00b5b873d957ca08c3669d;
 
-var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=` + city + `&limit=5&appid=c99c17905b00b5b873d957ca08c3669d`;
+
 
 //returns latitude and longitude
 function latLong(){
+    var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=` + city + `&limit=5&appid=c99c17905b00b5b873d957ca08c3669d`;
     fetch(geocodeUrl)
         .then(function(response){
             return response.json();
@@ -39,18 +44,16 @@ function latLong(){
         .then(function(data) {
             console.log(data);
             lat = data[0].lat;
-            long = data[0].lon;
-            console.log(lat);
-            console.log(long); 
+            long = data[0].lon; 
+            currentWeather();
             weatherApi(); 
         })
 }
 
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
-var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid={API key}'
-/*
+
 //current weather API cal 
 function currentWeather(){
+    var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
     fetch(currentUrl)
         .then(function(response){
             return response.json();
@@ -62,10 +65,11 @@ function currentWeather(){
             humidity = data.main.humidity;
             windSpeed = data.wind.windspeed; 
             weatherIcon = data.weather.icon;
+            console.log("check");
             displayCurrentWeather(); 
         })
 }
-*/
+
 
 // references for current weather in html
 var currentTempEl = document.querySelector('.currentTemp');
@@ -77,16 +81,18 @@ var currentDate = document.querySelector('.date');
 //function to display the current weather
 function displayCurrentWeather(){
     currentDate.textContent = moment().format("MM-DD-YY");
-    currentTempEl.textContent = temp;
-    currenthumidEl.textContent = humidity; 
-    currentWindEl.textContent = windSpeed; 
-    currentUVEl.textContent = UVindex;
-    currentIcon.setAttribute("src", weatherIcon);
+    currentTempEl.textContent = "Temperature: " + temp;
+    currenthumidEl.textContent = "Humidity: " + humidity; 
+    currentWindEl.textContent = "Wind speed: " + windSpeed + ' mph'; 
+    currentUVEl.textContent = "UV Index: " + UVindex;
+    var imgUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
+    currentIcon.setAttribute("src", imgUrl);
 }
-displayCurrentWeather();
-/*
+
+
  //returns weather for the 5 day forecast
  function weatherApi(){
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=c99c17905b00b5b873d957ca08c3669d';
      fetch(weatherUrl)
          .then(function(response){
              return response.json();
@@ -94,24 +100,23 @@ displayCurrentWeather();
          .then(function (data) {
              console.log(data);
              for (var i = 0; i < data.length; i++){
-                Ktemp = data.list[1].main.temp; 
+                Ktemp = data.list[i].main.temp; 
                 temp = Math.floor((Ktemp - 273)*(9/5) + 32); 
                 var newDate = moment().add(i + 1, 'days');
                 date = newDate.format("MM-DD-YY");
-                weatherIcon 
-                temp
-                windSpeed
-                humidity
-                display5day(i);
+                weatherIcon = data.list[i].weather.icon;
+                windSpeed = data.list[i].wind.speed;
+                humidity = data.list[i].main.humidity;
+                display5day(date, weatherIcon, temp, windSpeed, humidity);
              }
            
          })
  }
 
-*/
+
 
 // do this for each of the five days in the upcoming forecast
-function display5day(i){
+function display5day(date, weatherIcon, temp, windSpeed, humidity){
     var newDiv = document.createElement('div');
     // adding the date
     var new1 = document.createElement('p'); 
@@ -136,7 +141,7 @@ function display5day(i){
 
     // adding the wind
     var new4 = document.createElement('p'); 
-    var date4 = document.createTextNode(windSpeed); 
+    var date4 = document.createTextNode("Wind Speed: " + windSpeed + " mph"); 
     new4.appendChild(date4);
     newDiv.appendChild(new4);
     document.body.appendChild(newDiv); 
@@ -149,4 +154,4 @@ function display5day(i){
     document.body.appendChild(newDiv); 
   
 }
-display5day(5); 
+ 
